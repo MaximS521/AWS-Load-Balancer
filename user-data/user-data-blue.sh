e338a2c2a9ff20d73e2230304533de1d0214da32
MaximS521 <98423231+MaximS521@users.noreply.github.com>
2025-08-25 13:52:04 -0400

    Create user-data-blue.sh

---
+++ user-data/user-data-blue.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd awscli
+systemctl start httpd
+systemctl enable httpd
+
+mkdir -p /var/www/html/blue
+cd /var/www/html/blue
+
+aws s3 cp s3://arr-bucket-maxproject2-1234/hw-blue.css .
+aws s3 cp s3://arr-bucket-maxproject2-1234/hw-blue-py.css . || true
+aws s3 cp s3://arr-bucket-maxproject2-1234/python.png . || true
+aws s3 cp s3://arr-bucket-maxproject2-1234/apache.svg .
+aws s3 cp s3://arr-bucket-maxproject2-1234/blue-index.html ./index.html
+
+cd /var/www/html
+aws s3 cp s3://arr-bucket-maxproject2-1234/hw-blue.css .
+aws s3 cp s3://arr-bucket-maxproject2-1234/python.png . || true
+aws s3 cp s3://arr-bucket-maxproject2-1234/apache.svg .
+aws s3 cp s3://arr-bucket-maxproject2-1234/blue-root-index.html ./index.html || true
+
+systemctl restart httpd
