cf38170766255851931604e2eeff8ef7f1e93935
MaximS521 <98423231+MaximS521@users.noreply.github.com>
2025-08-25 14:16:34 -0400

    Update user-data-red.sh

--- user-data/user-data-red.sh
+++ user-data/user-data-red.sh
@@ -1,5 +1,3 @@
-Replace arr-bucket-maxproject2-1234 with your actual bucket name.
-
 #!/bin/bash
 set -e
 yum update -y
@@ -11,13 +9,13 @@ systemctl enable httpd
 mkdir -p /var/www/html/red
 cd /var/www/html/red
 
-aws s3 cp s3://arr-bucket-maxproject2-1234/hw-red.css .
-aws s3 cp s3://arr-bucket-maxproject2-1234/python.png . || true
-aws s3 cp s3://arr-bucket-maxproject2-1234/apache.svg .
-aws s3 cp s3://arr-bucket-maxproject2-1234/red-index.html ./index.html
+aws s3 cp s3://<YOUR-BUCKET-NAME>/hw-red.css .
+aws s3 cp s3://<YOUR-BUCKET-NAME>/python.png . || true
+aws s3 cp s3://<YOUR-BUCKET-NAME>/apache.svg .
+aws s3 cp s3://<YOUR-BUCKET-NAME>/red-index.html ./index.html
 
 # optional: set root index to red version for quick checks
 cd /var/www/html
-aws s3 cp s3://arr-bucket-maxproject2-1234/red-root-index.html ./index.html || true
+aws s3 cp s3://<YOUR-BUCKET-NAME>/red-root-index.html ./index.html || true
 
 systemctl restart httpd
